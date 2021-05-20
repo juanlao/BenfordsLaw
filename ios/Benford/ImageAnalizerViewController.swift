791fb969ac7de6c58571f6813fc85ac19f8690de
juanlao <juanlao@gmail.com>
2021-05-20 16:45:35 +0200

    Added image selector

--- ios/Benford/ImageAnalizerViewController.swift
+++ ios/Benford/ImageAnalizerViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ImageAnalizerViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker: ImagePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Image Analysis"
-        // Do any additional setup after loading the view.
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
     }
     
+    
+    @IBAction func selectImage(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
+    }
+   
+}
 
+extension ImageAnalizerViewController: ImagePickerDelegate{
+    func didSelect(image: UIImage?){
+        self.imageView.image = image
+    }
 }
